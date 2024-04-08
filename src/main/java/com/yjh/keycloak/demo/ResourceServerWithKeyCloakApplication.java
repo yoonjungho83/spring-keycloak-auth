b2eeb11b7a65cbda3cb183ed55be97b22fb27027
yoonjubgho83 <clubbboy@gmail.com>
2024-04-08 15:31:50 +0900

    keycloak 에서 받은 token을 keycloak server을 통해 유효성 검증 예제

---
+++ src/main/java/com/yjh/keycloak/demo/ResourceServerWithKeyCloakApplication.java
@@ -0,0 +1,13 @@
+package com.yjh.keycloak.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerWithKeyCloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerWithKeyCloakApplication.class, args);
+	}
+
+}
